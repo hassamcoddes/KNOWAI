292283197d87294e4d0ccafecc71a6f9e0e9393b
hassamcoddes <hassam.etrate@gmail.com>
2024-06-14 07:06:31 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const AutoSense = require('./AutoSense');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname)); // Serve static files from the root
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 app.post('/dimension', async (req, res) => {
     const { dimension, key, mediaInput } = req.body;
     try {
